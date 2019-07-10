6ec40e7c151d9a28c4cdcbe113a9904dfe40cf35
Sachin <sachidanand444@gmal.com>
2019-07-09 22:07:36 -0400

    Week3 Prob1
    Week3 Prob1

---
+++ Sachi/Algorithmic Toolbox/week3/MoneyChange1.java
@@ -0,0 +1,30 @@
+
+//The goal in this problem is to find the minimum number of coins needed to change 
+//the input value (an integer) into coins with denominations 1, 5, and 10.
+//-- Output Format. Output the minimum number of coins with denominations 1, 5, 10 that changes 𝑚.
+
+import java.util.Scanner;
+
+public class MoneyChange1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(calculateMoneyChangeFast(n));
+    }
+
+    private static int calculateMoneyChangeFast(int n) {
+        int[] denominations = {10, 5, 1};
+        int coins = 0, i = 0;
+        while (n > 0) {
+            int a = 0, sum = 0;
+            while (++a * denominations[i] <= n) {
+                sum = a * denominations[i];
+            }
+            coins = coins + a - 1;
+            n = n - sum;
+            i++;
+        }
+        return coins;
+    }
+}
