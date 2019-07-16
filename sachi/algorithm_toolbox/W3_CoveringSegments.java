2a93d5e0b8603aa50b9324e287f23fe50f7a5fbd
Sachin <sachidanand444@gmail.com>
2019-07-16 12:04:08 -0400

    Week3 Stuff

---
+++ sachi/algorithm_toolbox/W3_CoveringSegments.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class W3_CoveringSegments {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Segment[] segments = new Segment[n];
+        for (int i = 0; i < n; i++) {
+            int start, end;
+            start = scanner.nextInt();
+            end = scanner.nextInt();
+            segments[i] = new Segment(start, end);
+        }
+        int[] points = optimalPoints(segments);
+        System.out.println(points.length);
+        for (int point : points) {
+            System.out.print(point + " ");
+        }
+        scanner.close();
+    }
+
+    private static int[] optimalPoints(Segment[] segments) {
+        //TODO - Incomplete
+        int[] points = new int[segments.length];
+        return new int[]{1,2,3};
+    }
+
+    private static class Segment {
+        int start, end;
+
+        Segment(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+}
+ 
