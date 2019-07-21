887d3ee5407d99efd641e90e99304f9e6a5bf221
Sachin <sachidanand444@gmail.com>
2019-07-20 23:35:03 -0400

    Divide and Conquer - Theory

---
+++ sachi/algorithm_toolbox/P_PolynomialMul.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P_PolynomialMul {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[n];
+        // Set 1
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        // Set 2
+        for (int i = 0; i < n; i++) {
+            b[i] = scanner.nextInt();
+        }
+        printArray(naivePolyMult(a, b));
+        scanner.close();
+    }
+
+    /**
+     * Naive Imp i < 0 to n, j < 0 to n
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int[] naivePolyMult(int[] a, int[] b) {
+        int n = a.length;
+        int[] product = new int[2 * n - 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                product[i + j] += a[i] * b[j];
+            }
+        }
+        return product;
+    }
+
+    //TODO: Implement Karatsuba
+
+    public static void printArray(int[] a) {
+        Arrays.stream(a).forEach(item -> System.out.print(" " + item));
+    }
+
+}
---
+++ sachi/algorithm_toolbox/P_SortSelection.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*Selection Sort
+FInd least in the remaining array
+Move that to front
+Keep doing this for all records until you finish*/
+
+public class P_SortSelection {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        printArray(selectionSort(a));
+        scanner.close();
+    }
+
+    private static int[] selectionSort(int[] a) {
+        int n = a.length;
+        if (n == 1)
+            return a;
+        int minIndex;
+        boolean swap;
+        for (int i = 0; i < n; i++) {
+            minIndex = i;
+            swap = false;
+            for (int j = i; j < n; j++) {
+                if (a[j] < a[minIndex]) {
+                    minIndex = j;
+                    swap = true;
+                }
+            }
+            // Swap
+            if (swap) {
+                int temp = a[i];
+                a[i] = a[minIndex];
+                a[minIndex] = temp;
+            }
+        }
+        return a;
+    }
+
+    private static void printArray(int[] a) {
+        Arrays.stream(a).forEach(el -> System.out.print(el + " "));
+    }
+}
