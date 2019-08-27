ac57576df73b60215323a1d60aeb65ee1f743b9f
Sachin <sachidanand444@gmail.com>
2019-08-26 23:36:52 -0400

    Stacks

---
+++ sachi/leetcode/concepts/linkedlist/OddEvenLinkedList.java
@@ -0,0 +1,19 @@
+public class OddEvenLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode l1 = new ListNode(2);
+        ListNode l2 = new ListNode(3);
+        ListNode l3 = new ListNode(4);
+        ListNode l4 = new ListNode(5);
+        l4.next = null;
+        l3.next = l4;
+        l2.next = l3;
+        l1.next = l2;
+        head.next = l1;
+        oddEvenList(head).print(true);
+    }
+
+    private static ListNode oddEvenList(ListNode head) {
+        return null;
+    }
+}
---
+++ sachi/leetcode/concepts/stacks/MovingAverage.java
@@ -0,0 +1,35 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class MovingAverage {
+    private int size;
+    private Deque<Integer> queue;
+    private double sum;
+
+    private MovingAverage(int size) {
+        queue = new ArrayDeque<>();
+        this.size = size;
+        sum = 0;
+    }
+
+    public double next(int val) {
+        if (queue.size() < size) {
+            sum += val;
+            queue.addLast(val);
+            return sum / queue.size();
+        } else {
+            sum += val;
+            queue.addLast(val);
+            sum -= queue.pollFirst();
+            return sum / size;
+        }
+    }
+
+    public static void main(String[] args) {
+        MovingAverage movingAverage = new MovingAverage(3);
+        System.out.println(movingAverage.next(1));
+        System.out.println(movingAverage.next(10));
+        System.out.println(movingAverage.next(3));
+        System.out.println(movingAverage.next(5));
+    }
+}
