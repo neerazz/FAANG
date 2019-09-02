9162a4b6536bba1b9630b3fef3b6e3977e7b6015
Neeraj <b.neerajkumarsingh@gmail.com>
2019-09-01 23:02:45 -0400

    Binary Tree(Day 1 EOD)

---
+++ Neeraj/Leetcode/binarytree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -0,0 +1,18 @@
+package binarytree;
+
+/*
+https://leetcode.com/explore/learn/card/data-structure-tree/133/conclusion/942/
+Given inorder and postorder traversal of a tree, construct the binary tree.
+Note: You may assume that duplicates do not exist in the tree.
+For example, given
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+Return the following binary tree:
+    3
+   / \
+  9  20
+    /  \
+   15   7
+ */
+public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
+}
