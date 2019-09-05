1a4c0768c6fde3c95dde2ce2c26d8cd763176c39
Neeraj Kumar Singh B <43318996+neerazz@users.noreply.github.com>
2019-09-05 15:48:44 -0400

    Update FindPeakElement.java

--- Neeraj/Leetcode/binarysearch/FindPeakElement.java
+++ Neeraj/Leetcode/binarysearch/FindPeakElement.java
@@ -21,6 +21,18 @@ public class FindPeakElement {
         System.out.println("Answer is:" + findPeakElement(new int[]{1, 2, 3, 1}) + " should be [2].");
         System.out.println("Answer is:" + findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4}) + " should be [1 or 5].");
     }
+  
+    public int findPeakElement(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] > nums[mid + 1])
+                r = mid;
+            else
+                l = mid + 1;
+        }
+        return l;
+    }
 
     public static int findPeakElement(int[] nums) {
         return findPeakElementHelper(nums, 0, nums.length - 1);
