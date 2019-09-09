7d684f5d63c686c1e4f94b214445e04ce9c5a28a
Sachin <sachidanand444@gmail.com>
2019-09-09 16:31:53 -0400

    Linked List

--- sachi/concepts/util/Util.java
+++ sachi/concepts/util/Util.java
@@ -1,7 +1,35 @@
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class Util {
+
+    /**
+     * Generate array of size less than 100
+     * @return
+     */
+    public static int[] generateRandomArray() {
+        Random rand = new Random();
+        int size = rand.nextInt(100);
+        return generateRandomArray(size);
+    }
+
+    private static int[] generateRandomArray(int size) {
+        Random random = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(10000);
+        }
+        print(arr);
+        return arr;
+    }
+
+    public static void print(int[] array) {
+        System.out.println("\n---------- PRINTING Array-------------");
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+        System.out.println("\n---------- FInished Printing Array-------------");
+    }
 
     public static void print(int[][] matrix) {
         System.out.println("\n---------- PRINTING MATRIX-------------");
