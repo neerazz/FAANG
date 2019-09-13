f6d35880375d54c7cf887dd752493846592a9955
Neeraj <b.neerajkumarsingh@gmail.com>
2019-09-13 08:59:49 -0400

    Binary Search(Day 9 EOD)

--- Neeraj/Leetcode/binarysearch/FindKClosestElements.java
+++ Neeraj/Leetcode/binarysearch/FindKClosestElements.java
@@ -1,6 +1,8 @@
 package binarysearch;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
 https://leetcode.com/explore/learn/card/binary-search/135/template-iii/945/
@@ -18,10 +20,12 @@ Absolute value of elements in the array and x will not exceed 104
  */
 public class FindKClosestElements {
     public static void main(String[] args) {
-
+        System.out.println("Answer is:" + findClosestElements(new int[]{1, 2, 3, 4, 5}, 4, 3) + " should be [1,2,3,4].");
+        System.out.println("Answer is:" + findClosestElements(new int[]{1, 2, 3, 4, 5}, 4, -1) + " should be [1,2,3,4].");
+        System.out.println("Answer is:" + findClosestElements(new int[]{0, 0, 1, 2, 3, 3, 4, 7, 7, 8}, 3, 5) + " should be [3,3,4].");
     }
 
     public static List<Integer> findClosestElements(int[] arr, int k, int x) {
-        return null;
+        return Arrays.stream(arr).boxed().sorted((a, b) -> a.equals(b) ? a - b : Math.abs(a - x) - Math.abs(b - x)).collect(Collectors.toList()).subList(0, k).stream().sorted().collect(Collectors.toList());
     }
 }
