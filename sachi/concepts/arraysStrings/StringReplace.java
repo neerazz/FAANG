7a925edaf2af7a47b9406abc76fe678dc7ee1e21
Sachin <sachidanand444@gmail.com>
2019-09-17 22:27:40 -0400

    Change Formatting

---
+++ sachi/concepts/arraysStrings/StringReplace.java
@@ -0,0 +1,13 @@
+public class StringReplace {
+    public static void main(String[] args) {
+        System.out.println(replace("Thisisatemple is", "is"));
+    }
+
+    private static String replace(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : a.toCharArray()) {
+
+        }
+        return null;
+    }
+}
--- sachi/concepts/stacks-queues/NumberOfIslands.java
+++ sachi/concepts/stacks-queues/NumberOfIslands.java
@@ -2,6 +2,30 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+/*
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+
+Example 2:
+Input:
+11000
+11000
+00100
+00011
+Output: 3
+
+*/
+//TODO: Solve this again.
 
 public class NumberOfIslands {
     public static void main(String[] args) {
