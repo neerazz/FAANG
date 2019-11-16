3b6d353814c5a2f9a778eda8eeed12ce34a55a64
Neeraj Kumar Singh B <b.neerajkumarsingh@gmail.com>
2019-11-15 20:36:42 -0500

    Modified the project structure.

---
+++ SystemDesign/Neeraj/id-generation/src/main/java/com/neeraj/tinyurl/model/IdEntity.java
@@ -0,0 +1,42 @@
+package com.neeraj.tinyurl.model;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor @Builder
+@EntityListeners(AuditingEntityListener.class)
+public class IdEntity extends Auditable{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String uniqueId;
+
+    private String serviceName;
+
+    private LocalDateTime taken;
+
+    @CreatedDate
+    private LocalDateTime created;
+//    TODO find out how the CreatedDate works.
+}
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+abstract class Auditable {
+    @CreatedDate
+    @Temporal(TIMESTAMP)
+    protected Date creationDate;
+}
