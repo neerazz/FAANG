9b0d5acda5198ca1dfea6c9623b3e862ab32e494
Sachin <sachidanand444@gmail.com>
2020-01-20 23:04:50 -0500

    Trees

--- Algorithms/sachi/EPIJudge/epi_judge_java/epi/IsTreeBalanced.java
+++ Algorithms/sachi/EPIJudge/epi_judge_java/epi/IsTreeBalanced.java
@@ -8,8 +8,21 @@ public class IsTreeBalanced {
     @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
     public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-        // TODO - you fill in here.
-        return true;
+        if(tree == null) return true;
+        return isBalanced(tree.left, tree.right);
+    }
+
+    public static boolean isBalanced(BinaryTreeNode<Integer> leftTree, BinaryTreeNode<Integer> rightTree) {
+        int diff = height(leftTree,0) - height(rightTree,0);
+        return diff <= 1 && diff >= -1;
+    }
+
+    public static int height(BinaryTreeNode<Integer> node, int height){
+        if(node == null) return height;
+        ++height;
+        height(node.left, height);
+        height(node.right, height);
+        return height;
     }
 
     public static void main(String[] args) {
