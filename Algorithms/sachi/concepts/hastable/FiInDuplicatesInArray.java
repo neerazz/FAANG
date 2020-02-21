f9e77d22453d6231ec081cdee958606b9cbb523f
Sachin <sachidanand444@gmail.com>
2020-02-20 22:43:07 -0500

    Map

---
+++ Algorithms/sachi/concepts/hastable/FiInDuplicatesInArray.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FiInDuplicatesInArray {
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+    }
+
+    public static  int[] intersect(int[] nums1, int[] nums2) {
+
+        Map<Integer, Integer> counter = new HashMap<>();
+        List<Integer> sol = new ArrayList<>();
+
+        for (int i = 0; i < nums1.length; i++) {
+            if (counter.containsKey(nums1[i])) {
+                counter.put(nums1[i], counter.get(nums1[i]) + 1);
+            } else {
+                counter.put(i, 1);
+            }
+        }
+
+        for (int value : nums2) {
+            Integer val = counter.get(value);
+            if (val != null && val > 0) {
+                sol.add(value);
+                counter.put(value, --val);
+            }
+        }
+
+        int[] result = new int[sol.size()];
+        int j = 0;
+        for (Integer i : sol) {
+            result[j++] = i;
+        }
+
+        return result;
+
+    }
+}
---
+++ Algorithms/sachi/concepts/hastable/GroupShiftedStrings.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupShiftedStrings {
+
+    public static void main(String[] args) {
+        System.out.println(groupStrings(new String[]{"abc", "bcd", "acef", "xyz", "az", "ba", "a", "z"}));
+    }
+
+    public static List<List<String>> groupStrings(String[] strings) {
+
+        Map<String, List<String>> map = new HashMap<>();
+        List<String> list;
+        for (String str : strings) {
+            String hashValue = hash(str);
+            if (map.containsKey(hashValue)) {
+                list = map.get(hashValue);
+                list.add(str);
+            } else {
+                list = new ArrayList<>();
+                list.add(str);
+            }
+            map.put(hashValue, list);
+        }
+        return new ArrayList<>(map.values());
+
+    }
+
+    private static String hash(String str) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(str.length());
+        for (int i = 0; i < str.length() - 1; i++) {
+            int val = str.charAt(i + 1) - str.charAt(i);
+            val = val < 0 ? 26+val : val;
+            sb.append(val);
+        }
+        return sb.toString();
+    }
+}
