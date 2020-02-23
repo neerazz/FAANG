626e3b9c964a6cf6302e6f087b64e7560a9ca8df
Sachin <sachidanand444@gmail.com>
2020-02-22 23:29:51 -0500

    Some More Data

---
+++ Algorithms/sachi/concepts/primitives/SortByBits.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SortByBits {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortByBits(new int[]{10, 100, 1000, 10000})));
+    }
+
+
+    public static int[] sortByBits(int[] arr) {
+
+        Map<Integer, List<Integer>> counter = new HashMap<>();
+        List<Integer> elems;
+
+        for (int i : arr) {
+            String bin = Integer.toBinaryString(i);
+            int ones = count(bin);
+            if (counter.containsKey(ones)) {
+                elems = counter.get(ones);
+            } else {
+                elems = new ArrayList<>();
+            }
+            elems.add(i);
+            counter.put(ones, elems);
+        }
+
+        int[] keys = new int[counter.size()];
+        int j = 0;
+        for (Integer i : counter.keySet()) {
+            keys[j++] = i;
+        }
+
+        Arrays.sort(keys);
+
+        int[] sol = new int[arr.length];
+        int k = 0;
+        for (int y : keys) {
+            int p = 0;
+            List<Integer> values = counter.get(y);
+            int[] val = convertToArrayAndSort(values);
+            while (k < arr.length && p < val.length) {
+                sol[k++] = val[p++];
+            }
+        }
+        return sol;
+    }
+
+    private static int[] convertToArrayAndSort(List<Integer> list) {
+        int[] sol = new int[list.size()];
+        int j = 0;
+        for (Integer i : list) {
+            sol[j++] = i;
+        }
+        Arrays.sort(sol);
+        return sol;
+    }
+
+
+    private static int count(String bin) {
+        int count = 0;
+        for (int i = 0; i < bin.length(); i++) {
+            if (bin.charAt(i) == '1') count++;
+        }
+        return count;
+    }
+}
---
+++ Algorithms/sachi/lc/easy/Cashier.java
@@ -0,0 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Cashier {
+
+    int n = 0;
+    int counter = 0;
+    int discount = 0;
+    int[] products;
+    int[] prices;
+    Map<Integer, Integer> priceMap = new HashMap<>();
+
+    public Cashier(int n, int discount, int[] products, int[] prices) {
+        this.n = n;
+        this.discount = discount;
+        this.products = products;
+        this.prices = prices;
+        for (int i = 0; i < products.length; i++) {
+            priceMap.put(products[i], prices[i]);
+        }
+    }
+
+    public double getBill(int[] product, int[] amount) {
+        if (n == ++counter) {
+            //Apply discount
+            counter = 0;
+            double p = getPrice(product, amount);
+            return p - (p * discount / 100);
+        } else {
+            return getPrice(product, amount);
+        }
+
+    }
+
+    public double getPrice(int[] product, int[] qty) {
+        double p = 0;
+        for (int i = 0; i < product.length; i++) {
+            p += priceMap.get(product[i]) * qty[i];
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        Cashier cashier = new Cashier(3, 50, new int[]{1, 2, 3, 4, 5, 6, 7}, new int[]{100, 200, 300, 400, 300, 200, 100});
+        System.out.println(cashier.getBill(new int[]{1, 2}, new int[]{1, 2}));
+        System.out.println(cashier.getBill(new int[]{3, 7}, new int[]{10, 10}));
+        System.out.println(cashier.getBill(new int[]{1, 2, 3, 4, 5, 6, 7}, new int[]{1, 1, 1, 1, 1, 1, 1}));
+    }
+
+}
