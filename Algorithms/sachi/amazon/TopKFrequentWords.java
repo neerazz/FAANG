637742385adf89853d74c4c32676f1ab4391273e
Sachin <sachidananda.singh@windstream.com>
2020-03-10 11:47:47 -0400

    Top K Words

--- Algorithms/sachi/amazon/TopKFrequentWords.java
+++ Algorithms/sachi/amazon/TopKFrequentWords.java
@@ -18,15 +18,15 @@ with the number of occurrence being 4, 3, 2 and 1 respectively.
 
 */
 
-import java.util.*;
 import java.util.PriorityQueue;
+import java.util.*;
 
 class TopKFrequentWords {
 
     public static void main(String[] args) {
-        String[] input = {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
-        //String[] input = {"i", "love", "leetcode", "i", "love", "coding"};
-        System.out.println(topKFrequent(input, 2));
+        //String[] input = {"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"};
+        String[] input = {"i", "love", "leetcode", "i", "love", "coding"};
+        System.out.println(topKFrequent(input, 4));
     }
 
 
@@ -38,25 +38,28 @@ class TopKFrequentWords {
         }
 
         PriorityQueue<String> pq = new PriorityQueue<>((w1, w2) -> {
-			if (counter.get(w1).equals(counter.get(w2))) {
-				return w1.compareTo(w2);
-			} else {
-				return counter.get(w1) - counter.get(w2);
-			}
-		});
+            if (counter.get(w1).equals(counter.get(w2))) {
+                return w2.compareTo(w1);
+            } else {
+                return counter.get(w1) - counter.get(w2);
+            }
+        });
 
-        for(String str : counter.keySet()){
-        	pq.add(str);
-        	if(pq.size() > k){
-        		pq.poll();
-			}
-		}
-		List<String> sol = new ArrayList<>();
-        while (!pq.isEmpty()){
-        	sol.add(pq.poll());
-		}
-        //Collections.reverse(sol);
+        for (String s : counter.keySet()) {
+            pq.offer(s);
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+
+        List<String> sol = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            sol.add(pq.poll());
+        }
+        Collections.reverse(sol);
         return sol;
+
     }
 
 }
