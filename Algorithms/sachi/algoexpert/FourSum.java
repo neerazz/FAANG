f97e29567c556d730a4c45edd6f58a009e37d880
Sachin <sachidananda.singh@windstream.com>
2020-03-17 12:54:02 -0400

    Algo

---
+++ Algorithms/sachi/algoexpert/FourSum.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum {
+
+    public static List<Integer[]> fourNumberSum(int[] arr, int targetSum) {
+        List<Integer[]> sol = new ArrayList<>();
+        //Sort the arrays
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 3; i++) {
+            for (int j = i + 1; j < arr.length - 2; j++) {
+                int start = j + 1, end = arr.length - 1;
+                int newTarget = targetSum - arr[i] - arr[j];
+                while (start < end) {
+                    int sum = arr[start] + arr[end];
+                    if (newTarget == sum) {
+                        sol.add(new Integer[]{arr[i], arr[j], arr[start], arr[end]});
+                        start++;
+                        end--;
+                    } else if (sum > newTarget) {
+                        end--;
+                    } else {
+                        start++;
+                    }
+                }
+            }
+        }
+        return sol;
+    }
+
+    public static void main(String[] args) {
+        List<Integer[]> sol = fourNumberSum(new int[]{1, 2, 3, 4, 5, 6, 7}, 10);
+        for (Integer[] intarray : sol) {
+            System.out.println(Arrays.toString(intarray));
+        }
+    }
+}
--- Algorithms/sachi/concepts/binaryTree/traversals/InOrderTraversal.java
+++ Algorithms/sachi/concepts/binaryTree/traversals/InOrderTraversal.java
@@ -1,24 +1,42 @@
-class InOrderTraversal{
+package traversals;
 
-	public List<Integer> inorderTraversal(TreeNode root) {
-    	List<Integer> sol = new ArrayList<>();
-    	inorderTraversalRecur(root, sol);
-    	return sol;	    
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+class InOrderTraversal {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> sol = new ArrayList<>();
+        inorderTraversalRecur(root, sol);
+        return sol;
     }
 
     public void inorderTraversalRecur(TreeNode root, List<Integer> sol) {
-    	if(root == null) return;
-    	inorderTraversalRecur(root.left, sol);
-    	sol.add(root.val);
-    	inorderTraversalRecur(root.right, sol);  
+        if (root == null) return;
+        inorderTraversalRecur(root.left, sol);
+        sol.add(root.val);
+        inorderTraversalRecur(root.right, sol);
     }
 
-    public  List<Integer> inOrderTraversalIter(TreeNode root){
+    public List<Integer> inOrderTraversalIter(TreeNode root) {
         List<Integer> sol = new ArrayList<>();
-		Deque<TreeNode> stack = new LinkedList<>();
-		stack.add(root);
-		while(!stack.isEmpty()){
-			
-		}
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+
+        }
+        return null;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
     }
 }
