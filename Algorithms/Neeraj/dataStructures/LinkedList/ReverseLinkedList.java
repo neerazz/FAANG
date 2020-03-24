330e177db88892bb9176a511e18ed3f938b39036
neerajkumarsinghb <b.neerajkumarsingh@gmail.com>
2020-03-24 00:31:01 -0400

    March 24th EOD

--- Algorithms/Neeraj/dataStructures/LinkedList/ReverseLinkedList.java
+++ Algorithms/Neeraj/dataStructures/LinkedList/ReverseLinkedList.java
@@ -8,9 +8,21 @@ A linked list can be reversed either iteratively or recursively. Could you imple
  */
 public class ReverseLinkedList {
     public static void main(String[] args) {
-        System.out.println(reverseList(RemoveNthNodeFromEndOfList.createListNode(new int[]{1, 2, 3, 4, 5})));
+        System.out.println(reverseList(createListNode(new int[]{1, 2, 3, 4, 5})));
+        System.out.println(reverseList(createListNode(new int[]{4,5,6,7,8})));
+        System.out.println(reverseList2(createListNode(new int[]{1, 2, 3, 4, 5})));
+        System.out.println(reverseList2(createListNode(new int[]{4,5,6,7,8})));
     }
 
+    public static ListNode createListNode(int[] nums) {
+        if (nums.length == 0) return null;
+        ListNode output = new ListNode(nums[0]), current = output;
+        for (int i = 1; i < nums.length; i++) {
+            current.next = new ListNode(nums[i]);
+            current = current.next;
+        }
+        return output;
+    }
     /*
         Solution:
         Step 1: Take a temporary node and the output node. Take the first number and set it next to second and set that to output. (From 1->2 to 2->1)
@@ -25,5 +37,29 @@ public class ReverseLinkedList {
             current = temp;
         }
         return output;
+    }
+    public static ListNode reverseList2(ListNode head) {
+        ListNode output = new ListNode(-1), outputRef = output;
+        while (head != null) {
+            ListNode temp = outputRef.next;
+            outputRef.next = new ListNode(head.data);
+            outputRef.next.next = temp;
+            head = head.next;
+        }
+        return output.next;
+    }
+    static class ListNode {
+        int data;
+        ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        @Override
+        public String toString() {
+            return "" + data + ":->"+ next;
+        }
     }
 }
