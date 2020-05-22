349e98b330b2dd3246f0d1bdbff1f9f7dd1aac83
neerajkumarsinghb <b.neerajkumarsingh@gmail.com>
2020-05-22 10:52:10 -0400

    Moded system design to a different repo

--- SystemDesign/Sachi/tinyurl/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-/*insert into TinyURL (id, shorturl, longurl, user_id, hits, create_date, expiry_date)
-values (100, 'qwerty', 'http://google.com', '213121', 0,
-        '2019-10-19 21:57:00', '2019-10-19 23:00:00');*/
