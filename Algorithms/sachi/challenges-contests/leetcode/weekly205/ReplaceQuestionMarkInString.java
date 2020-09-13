e4a1ec6cc783f850fc46b54ca001e373e215904a
Sachin <sachidanand444@gmail.com>
2020-09-13 18:17:46 -0400

    Code

---
+++ Algorithms/sachi/challenges-contests/leetcode/weekly205/ReplaceQuestionMarkInString.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReplaceQuestionMarkInString {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String modifyString(String s) {
+        int[] indices = new int[26];
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<indices.length; i++){
+            indices[i] = i;
+            map.put(i,i);
+        }
+        for(char c : s.toCharArray()){
+
+        }
+        return null;
+    }
+}
