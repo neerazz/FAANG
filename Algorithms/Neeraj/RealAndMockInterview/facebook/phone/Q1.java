755e51267d13ff7be809f188c3400dc23ccaeccb
Neeraj Kumar Singh B <43318996+neerazz@users.noreply.github.com>
2020-11-02 13:32:51 -0500

    Added microsoft questions

--- Algorithms/Neeraj/RealAndMockInterview/facebook/phone/Q1.java
+++ Algorithms/Neeraj/RealAndMockInterview/facebook/phone/Q1.java
@@ -9,18 +9,21 @@ Put the code - Undo
 //This is two fixed arrays though
 */
 
-static int  findRight(int[] A, int key) {
-  int start = 0, end = arr.length - 1;
-  while (start < end) {
-      int mid = start + (end - start) / 2;
-      if (arr[mid] == key) {
-          if (mid == arr.length - 1 || arr[mid + 1] != key) {
-              result[1] = mid;
-              return result;
-          }
-          start = mid + 1;
-      }
-      else if (arr[mid] < key) start = mid + 1;
-      else end = mid;
-  }
+class Q1 {
+    static int[] findRight(int[] arr, int key) {
+        int[] result = new int[2];
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == key) {
+                if (mid == arr.length - 1 || arr[mid + 1] != key) {
+                    result[1] = mid;
+                    return result;
+                }
+                start = mid + 1;
+            } else if (arr[mid] < key) start = mid + 1;
+            else end = mid;
+        }
+        return result;
+    }
 }
---
+++ Algorithms/Neeraj/RealAndMockInterview/microsoft/OnlineAssesment/MinAdjSwapsToGroupRedBalls.java
@@ -0,0 +1,43 @@
+package OnlineAssesment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created on:  Oct 31, 2020
+ * Questions: https://leetcode.com/discuss/interview-question/414660/
+ */
+
+public class MinAdjSwapsToGroupRedBalls {
+
+    public static void main(String[] args) {
+        System.out.println(minDistance("WRRWWR"));
+        System.out.println(minDistance("WWRWWWRWR"));
+        System.out.println(minDistance("WWW"));
+        System.out.println(minDistance("RW"));
+        System.out.println(minDistance("RW".repeat(100000)));
+    }
+
+    private static int minDistance(String s) {
+        List<Integer> indexes = getRedIndexes(s);
+        if (indexes.size() < 2) return 0;
+        int swaps = 0, mid = indexes.size() / 2;
+        for (int i = 0; i < indexes.size(); i++) {
+            int actualDistance = Math.abs(indexes.get(mid) - indexes.get(i));
+            int expectedDistance = Math.abs(mid - i);
+            swaps += actualDistance - expectedDistance;
+            if (swaps > 1_000_000_000) return -1;
+        }
+        return swaps;
+    }
+
+    private static List<Integer> getRedIndexes(String s) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'R') {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
