15f6edfb8479e220bdfd04ea2b065b2a84d2228a
Neeraj Kumar Singh B <43318996+neerazz@users.noreply.github.com>
2021-01-16 20:52:55 -0500

    Added top amazon problems.

---
+++ Algorithms/Neeraj/topAmazonQuestions/LoadBalancer.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Created on:  Jan 16, 2021
+ * Questions: https://aonecode.com/amazon-online-assessment-load-balancer
+ */
+
+public class LoadBalancer {
+
+    public static void main(String[] args) {
+//        System.out.println(loadBalance(new int[]{1, 3, 4, 2, 2, 2, 1, 1, 2}));
+        System.out.println(loadBalance(new int[]{1, 2, 1, 2, 1}));
+    }
+
+    public static boolean loadBalance(int[] arr) {
+        int len = arr.length, sTree[] = new int[2 * len + 1], start = 2, end = len - 2;
+        if (len < 5) return false;
+        buildTree(arr, 0, len - 1, sTree, 0);
+        for (int i = start; i < end; i++) {
+            int sum = 0;
+            for (int j = i; j < end; j++) {
+                sum += arr[j];
+                int left = query(0, i - 2, sTree, 0, len - 1, 0);
+                int right = query(j + 2, len - 1, sTree, 0, len - 1, 0);
+                if (sum == left && sum == right) return true;
+            }
+        }
+        return false;
+    }
+
+    private static int query(int qs, int qe, int[] sTree, int s, int e, int i) {
+        if (s > qe || e < qs) return 0;
+        if (qs <= s && e <= qe) return sTree[i];
+        int m = (s + e) / 2;
+        return query(qs, qe, sTree, s, m, 2 * i + 1) + query(qs, qe, sTree, m + 1, e, 2 * i + 2);
+    }
+
+    private static void buildTree(int[] arr, int s, int e, int[] sTree, int idx) {
+        if (s == e) {
+            sTree[idx] = arr[s];
+        } else {
+            int mid = (s + e) / 2, left = 2 * idx + 1, right = 2 * idx + 2;
+            buildTree(arr, s, mid, sTree, left);
+            buildTree(arr, mid + 1, e, sTree, right);
+            sTree[idx] = sTree[left] + sTree[right];
+        }
+    }
+}
---
+++ Algorithms/Neeraj/topAmazonQuestions/OptimizeMemoryUsage.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Created on:  Jan 16, 2021
+ * Questions: https://aonecode.com/amazon-online-assessment-oa2-optimize-memory-usage
+ */
+
+public class OptimizeMemoryUsage {
+
+    public static void main(String[] args) {
+        optimizeMemoryUsage(new int[]{1, 7, 2, 4, 5, 6}, new int[]{1, 1, 2}, 0).forEach(i -> System.out.println(Arrays.toString(i)));
+//        optimizeMemoryUsage(new int[]{6, 6, 6}, new int[]{1}, 7).forEach(i -> System.out.println(Arrays.toString(i)));
+//        optimizeMemoryUsage(new int[]{1, 1}, new int[]{6, 6, 6, 6, 7}, 7).stream().forEach(i -> System.out.println(Arrays.toString(i)));
+//        optimizeMemoryUsage(new int[]{1, 7, 2, 4, 5, 6}, new int[]{1, 1, 2}, 1).stream().forEach(i -> System.out.println(Arrays.toString(i)));
+//        optimizeMemoryUsage(new int[]{1, 7, 8}, new int[]{3, 1, 2}, 10).stream().forEach(i -> System.out.println(Arrays.toString(i)));
+    }
+
+    public static List<int[]> optimizeMemoryUsage(int[] fore, int[] back, int k) {
+        int max = 0;
+        TreeMap<Integer, Set<Integer>> fMap = new TreeMap<>((v1, v2) -> Integer.compare(v2, v1)), bMap = new TreeMap<>();
+        Map<Integer, List<int[]>> pairs = new HashMap<>();
+        for (int i = 0; i < back.length; i++) {
+            int cur = back[i];
+            bMap.computeIfAbsent(cur, val -> new HashSet<>()).add(i);
+            pairs.computeIfAbsent(cur, val -> new ArrayList<>()).add(new int[]{-1, i});
+            if (cur <= k) max = Math.max(max, cur);
+        }
+        for (int i = 0; i < fore.length; i++) {
+            int cur = fore[i];
+            pairs.computeIfAbsent(cur, val -> new ArrayList<>()).add(new int[]{i, -1});
+            fMap.computeIfAbsent(cur, val -> new HashSet<>()).add(i);
+            if (cur <= k) max = Math.max(max, cur);
+        }
+        for (int a : fMap.keySet()) {
+            Map.Entry<Integer, Set<Integer>> bEntry = bMap.floorEntry(k - a);
+            if (bEntry != null) {
+                Set<Integer> aIdxs = fMap.get(a);
+                int sum = a + bEntry.getKey();
+                if (sum <= k) {
+                    max = Math.max(max, sum);
+                    for (int aId : aIdxs) {
+                        for (int bId : bEntry.getValue()) {
+                            pairs.computeIfAbsent(sum, val -> new ArrayList<>()).add(new int[]{aId, bId});
+                        }
+                    }
+                }
+            }
+        }
+        return pairs.getOrDefault(max, Arrays.asList(new int[]{-1,-1}));
+    }
+}
