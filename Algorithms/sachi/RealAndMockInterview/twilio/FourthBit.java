48e3b125be6c637edac35379835fdaaa5d8ffa81
Sachin <sachidanand444@gmail.com>
2021-02-20 20:18:20 -0500

    Twilio Codes

---
+++ Algorithms/sachi/RealAndMockInterview/twilio/FourthBit.java
@@ -0,0 +1,13 @@
+public class FourthBit {
+
+    public static void main(String[] args) {
+        int n = 23;
+        System.out.println(Integer.toBinaryString(n));
+        System.out.println(fourthBit(n));
+    }
+
+    private static int fourthBit(int n) {
+        n = n >> 3;
+        return n & 1;
+    }
+}
---
+++ Algorithms/sachi/RealAndMockInterview/twilio/MissingWords.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MissingWords {
+    public static void main(String[] args) {
+        String s = "i like cheese";
+        String t = "like";
+        System.out.println(Arrays.toString(missingwords(s, t)));
+        System.out.println(missingWords(s, t));
+    }
+
+    public static List<String> missingWords(String s, String t) {
+        String[] parts = s.split(" ");
+        String[] otherparts = t.split(" ");
+        List<String> missing = new ArrayList<>();
+        for (int i = 0, j = 0; i < parts.length; i++) {
+            if (j < otherparts.length && parts[i].equals(otherparts[j])) {
+                j++;
+            } else {
+                missing.add(parts[i]);
+            }
+        }
+        return missing;
+    }
+
+    public static String[] missingwords(String t, String s) {
+        for (String word : s.split(" ")) {
+            String[] a = t.split(word, 2);
+            t = a[0].trim() + a[1];
+        }
+        return t.split(" ");
+    }
+
+}
