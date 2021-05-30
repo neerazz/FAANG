924528e8b431756edaecf974dd8377ac41b9468f
Sachin <sachidanand444@gmail.com>
2021-05-30 19:16:53 -0400

    Amazon

---
+++ Algorithms/sachi/RealAndMockInterview/amazon/OptimizingBoxWeights.java
@@ -0,0 +1,34 @@
+package amazon;
+
+public class OptimizingBoxWeights {
+
+    public static List<Integer> optimizingBoxWeights(List<Integer> arr) {
+        // WRITE YOUR BRILLIANT CODE HERE
+        int target = arr.stream().reduce(0, Integer::sum) / 2;
+        return List.of();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
+            return b - a;
+        });
+        pq.addAll(arr);
+        int curSum = 0;
+        List<Integer> res = new ArrayList<>();
+        while (curSum <= target) {
+            int val = pq.poll();
+            curSum += val;
+            res.add(val);
+        }
+        Collections.reverse(res);
+        return res;
+    }
+    public static List<String> splitWords(String s) {
+        return s.isEmpty() ? List.of() : Arrays.asList(s.split(" "));
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> arr = splitWords(scanner.nextLine()).stream().map(Integer::parseInt).collect(Collectors.toList());
+        scanner.close();
+        List<Integer> res = optimizingBoxWeights(arr);
+        System.out.println(res.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+    }
+
+}
