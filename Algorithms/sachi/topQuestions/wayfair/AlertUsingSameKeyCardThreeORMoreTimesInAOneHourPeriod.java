6bac9eab7776cb37ce3d7e5a2802f5cbf1393a68
Sachin <sachidanand444@gmail.com>
2021-07-27 23:25:29 -0400

    Wayfair Questions

--- Algorithms/sachi/topQuestions/wayfair/AlertUsingSameKeyCardThreeORMoreTimesInAOneHourPeriod.java
+++ Algorithms/sachi/topQuestions/wayfair/AlertUsingSameKeyCardThreeORMoreTimesInAOneHourPeriod.java
@@ -1,8 +1,22 @@
 package topQuestions.wayfair;
 
-import java.security.AccessControlException;
-import java.sql.SQLOutput;
 import java.util.*;
+
+/*
+https://leetcode.com/problems/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/
+F
+
+LeetCode company workers use key-cards to unlock office doors. Each time a worker uses their key-card, the security
+system saves the worker's name and the time when it was used. The system emits an alert if any worker uses the key-card
+three or more times in a one-hour period. You are given a list of strings keyName and keyTime where
+[keyName[i], keyTime[i]] corresponds to a person's name and the time when their key-card was used in a single day.
+
+Access times are given in the 24-hour time format "HH:MM", such as "23:51" and "09:49".
+Return a list of unique worker names who received an alert for frequent keycard use.
+Sort the names in ascending order alphabetically.
+Notice that "10:00" - "11:00" is considered to be within a one-hour period, while "22:51" - "23:52"
+is not considered to be within a one-hour period.
+ */
 
 public class AlertUsingSameKeyCardThreeORMoreTimesInAOneHourPeriod {
 
