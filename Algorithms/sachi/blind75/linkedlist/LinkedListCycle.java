d8eddd56e9fefa70d62c5e4548dd1546b90acd55
Sachin <sachidanand444@gmail.com>
2021-08-08 18:05:25 -0400

    Linked List Questions

---
+++ Algorithms/sachi/blind75/linkedlist/LinkedListCycle.java
@@ -0,0 +1,45 @@
+package blind75.linkedlist;
+
+import util.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+https://leetcode.com/problems/linked-list-cycle/
+P
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+*/
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
+
+    public boolean hasCycleNaive(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) return true;
+            set.add(head);
+            head = head.next;
+        }
+        return false;
+    }
+}
